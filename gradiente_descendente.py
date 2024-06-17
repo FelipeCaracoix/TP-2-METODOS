6141c2cf85f74f4ab04eaeb9e36b17ff7789e1ea
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2024-06-17 16:28:24 -0300

    ordenanando ando

--- gradiente_descendente.py
+++ gradiente_descendente.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Máxima cantidad de iteraciones (previene loops infinitos)
-MAX_ITER            = 1000
+MAX_ITER = 1000
 
 # Criterio de convergencia (identifica un "plateau")
 TOLERANCIA = 0.0001
@@ -15,13 +15,13 @@ def df(x):
 # Función de búsqueda del mínimo de f(x)
 def gradiente_descendente(x_0):
     # Valor inicial de x
-    x_t   = x_0
+    x_t = x_0
 
     # Factor de escala de la derivada de f (hiperparámetro)
     alpha = 0.1
 
     # Mientras no hayamos llegado al maximo de iteraciones
-    iter  = 0
+    iter = 0
 
     while iter <= MAX_ITER:
         print("Iteración: ", iter, "- Mínimo alcanzado hasta el momento: ", f(x_t))
@@ -30,7 +30,7 @@ def gradiente_descendente(x_0):
 
         # Chequeamos si ya alcanzamos la convergencia
         if abs(f(x_tsig) - f(x_t)) < TOLERANCIA:
-          break
+            break
 
         # Preparamos la siguiente iteración
         x_t = x_tsig
