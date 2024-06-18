57e92fa0f9c7ca120fdaa2344aeb355cea073306
victoriamarsili <victoriamarsili@gmail.com>
2024-06-18 20:23:25 -0300

    derivadas con respecto a la ec (4) sin sumatoria

--- descxgrad.py
+++ descxgrad.py
@@ -24,8 +24,20 @@ def abrirImagenesEscaladas( carpeta, escala=32 ):
 
     return imagenes
 
-i = np.array(abrirImagenesEscaladas("/Users/victoriamarsili/Downloads/chest_xray/test/PNEUMONIA", escala=32))
-print(i.shape)
+# Función para cargar imágenes y etiquetas desde una carpeta dada
+def cargar_datos(carpeta_base, escala=32):
+    pneumonia_path = os.path.join(carpeta_base, "PNEUMONIA")
+    normal_path = os.path.join(carpeta_base, "NORMAL")
+
+    # Cargar imágenes y etiquetas
+    pneumonia_images = abrirImagenesEscaladas(pneumonia_path, escala)
+    normal_images = abrirImagenesEscaladas(normal_path, escala)
+
+    X = np.hstack((pneumonia_images, normal_images))
+    y = np.array([1] * pneumonia_images.shape[1] + [0] * normal_images.shape[1])
+
+    return X, y
+
 d = np.random.randn(1024)
 def balancear_datos(imagenes_entrenamiento):
     b = np.random.randn(1)
@@ -33,5 +45,22 @@ def balancear_datos(imagenes_entrenamiento):
     imagenes_entrenamiento_balanceadas = gradiente_descendente(w,b,imagenes_entrenamiento,d)
     return imagenes_entrenamiento_balanceadas
 
-w_estrella, b_estrella = balancear_datos(i)
-print(funcion_f(i,w_estrella, b_estrella, d))
+#w_estrella, b_estrella = balancear_datos(i)
+#print(funcion_f(i,w_estrella, b_estrella, d))
+
+# Cargar imágenes y datos
+train_images, d_entrenamiento = cargar_datos("/Users/victoriamarsili/Downloads/chest_xray", escala=32)
+test_images, d_test = cargar_datos("/path/to/dataset/test", escala=32)
+
+# Ejecutar el balanceo de datos y obtener errores
+w_estrella, b_estrella, train_errors, test_errors = gradiente_descendente(
+    np.random.randn(train_images.shape[0]), np.random.randn(1), train_images, d_entrenamiento, test_images, d_test)
+
+# Graficar errores
+plt.plot(train_errors, label='Error de Entrenamiento')
+plt.plot(test_errors, label='Error de Prueba')
+plt.xlabel('Iteraciones')
+plt.ylabel('Error Cuadrático Medio')
+plt.title('Evolución del Error durante el Entrenamiento')
+plt.legend()
+plt.show()
