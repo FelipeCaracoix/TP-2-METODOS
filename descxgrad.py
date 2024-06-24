70132091b8d3f840e67d1cdbbdda97a3a1634dc0
victoriamarsili <victoriamarsili@gmail.com>
2024-06-24 10:23:49 -0300

    last

--- descxgrad.py
+++ descxgrad.py
@@ -1,62 +1,64 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 import os
-from gradiente_descendente import gradiente_descendente, funcion_f
+from PIL import Image
+from gradiente_descendente import gradiente_descendente
 
-def abrirImagenesEscaladas( carpeta, escala=32 ):
-    # abre todas las imagenes de la carpeta, y las escala de tal forma que midan (escala x escala)px
-    # devuelve las imagenes aplanadas -> vectores de tamano escala^2 con valores entre 0 y 1
+def abrirImagenesEscaladas(carpeta, escala=32):
     imagenes = []
+    etiquetas = []
 
     for dirpath, dirnames, filenames in os.walk(carpeta):
         for file in filenames:
-            img = Image.open( os.path.join(carpeta, file) )
+            img = Image.open(os.path.join(carpeta, file))
             img = img.resize((escala, escala))
-            img.convert('1')
-            img = np.asarray(img)
-            if len(img.shape)==3:
-                img = img[:,:,0].reshape((escala**2 )) / 255
+            img = img.convert('L')  # Convertir a escala de grises
+            img = np.asarray(img).reshape((escala ** 2)) / 255
+
+            # Asignar etiquetas basadas en el nombre del archivo
+            if "virus" in file or "bacteria" in file:
+                etiqueta = 1
             else:
-                img = img.reshape((escala**2 )) / 255
-            
-            imagenes.append( img )
+                etiqueta = 0
 
-    return imagenes
+            imagenes.append(img)
+            etiquetas.append(etiqueta)
 
-# Función para cargar imágenes y etiquetas desde una carpeta dada
+    return imagenes, etiquetas
+
 def cargar_datos(carpeta_base, escala=32):
     pneumonia_path = os.path.join(carpeta_base, "PNEUMONIA")
     normal_path = os.path.join(carpeta_base, "NORMAL")
 
-    # Cargar imágenes y etiquetas
-    pneumonia_images = abrirImagenesEscaladas(pneumonia_path, escala)
-    normal_images = abrirImagenesEscaladas(normal_path, escala)
+    pneumonia_images, pneumonia_labels = abrirImagenesEscaladas(pneumonia_path, escala)
+    normal_images, normal_labels = abrirImagenesEscaladas(normal_path, escala)
 
-    X = np.hstack((pneumonia_images, normal_images))
-    y = np.array([1] * pneumonia_images.shape[1] + [0] * normal_images.shape[1])
+    # Unir las imágenes y etiquetas
+    X = pneumonia_images + normal_images
+    y = pneumonia_labels + normal_labels
 
-    return X, y
+    return np.array(X), np.array(y)
 
-d = np.random.randn(1024)
-def balancear_datos(imagenes_entrenamiento):
+def balancear_datos(imagenes_entrenamiento, etiquetas_entrenamiento):
     b = np.random.randn(1)
     w = np.random.randn(390)
-    imagenes_entrenamiento_balanceadas = gradiente_descendente(w,b,imagenes_entrenamiento,d)
-    return imagenes_entrenamiento_balanceadas
-
-#w_estrella, b_estrella = balancear_datos(i)
-#print(funcion_f(i,w_estrella, b_estrella, d))
+    imagenes_entrenamiento_balanceadas, errores = gradiente_descendente(w, b, imagenes_entrenamiento, etiquetas_entrenamiento)
+    return imagenes_entrenamiento_balanceadas, errores
 
 # Cargar imágenes y datos
-train_images, d_entrenamiento = cargar_datos("/Users/victoriamarsili/Downloads/chest_xray", escala=32)
-test_images, d_test = cargar_datos("/path/to/dataset/test", escala=32)
+train_images, d_entrenamiento = cargar_datos("/Users/victoriamarsili/Downloads/chest_xray/train", escala=32)
+test_images, d_test = cargar_datos("/Users/victoriamarsili/Downloads/chest_xray/test", escala=32)
+
+# Inicializar d a partir de los nombres de los archivos
+d_entrenamiento = np.array([1 if "virus" in fname or "bacteria" in fname else 0 for fname in d_entrenamiento])
+d_test = np.array([1 if "virus" in fname or "bacteria" in fname else 0 for fname in d_test])
 
 # Ejecutar el balanceo de datos y obtener errores
 w_estrella, b_estrella, train_errors, test_errors = gradiente_descendente(
-    np.random.randn(train_images.shape[0]), np.random.randn(1), train_images, d_entrenamiento, test_images, d_test)
+    np.random.randn(train_images.shape[1]), np.random.randn(1), train_images, d_entrenamiento, test_images, d_test)
 
 # Graficar errores
+import matplotlib.pyplot as plt
+
 plt.plot(train_errors, label='Error de Entrenamiento')
 plt.plot(test_errors, label='Error de Prueba')
 plt.xlabel('Iteraciones')
---
+++ error.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from formula import suma_derivada, suma_f
+
+# Ejercicio 3: función para calcular el error cuadrático
+def error_cuadratico():
+    
