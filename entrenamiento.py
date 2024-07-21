ffad9b452e43d21a43522a35af2018c9c49a30b0
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2024-07-21 00:35:09 -0300

    traino nice

--- entrenamiento.py
+++ entrenamiento.py
@@ -7,33 +7,30 @@ from ej6 import matriz_confusion
 
 def main():
     # 4x4s177, 8x8s79, 16x16s171, 32x32s150, 64x64s123, 128x128s105, 256x256s42
-    #array = [(4,177),(8,79),(16,171),(32,150),(64,123),(128,105),(256,42)]
-    # (4,177,0.0001),(8,79,0.0001),(16,171,0.0001),(32,150,0.0001),(64,123,0.001),
-    array = [(64,168)]
-    # seeds = range(1,50)
-    # for seed in seeds:
-    # 0.0000001,0.000001,0.00001,0.0001, 0.001, 0.01
-    alphas= [0.0001]
-    for alpha in alphas:
-        for Esc in array:
-            escala = Esc[0]
-            seed = Esc[1]
+    array = [(4,177),(8,79),(16,171),(32,150),(64,123),(128,105),(256,42)]
+    seeds = range(1,50)
+    for seed in seeds:
+        alphas= [0.0000001,0.000001,0.00001,0.0001, 0.001, 0.01]
+        for alpha in alphas:
+            for Esc in array:
+                escala = Esc[0]
+                seed = Esc[1]
 
-            images, d = cargar_datos("/Users/nicolasfranke/Downloads/chest_xray/train/all", escala)#Poner path a una carpeta con todas las immagenes de train
-            images_test, d_test =  cargar_datos("/Users/nicolasfranke/Downloads/chest_xray/test/all", escala)
-            np.random.seed(seed)
-            b = np.random.randn(1)
-            w = np.random.randn(images[0].shape[0])
-            print("imagenes cargadas")
-            images_balanceadas, d_balanceado = balancear_datos(images, d)
-            w_estrella, b_estrella, errores_test, errores_train = gradiente_descendente(w, b, images_balanceadas, d_balanceado, alpha,images_test, d_test)
+                images, d = cargar_datos("/Users/nicolasfranke/Downloads/chest_xray/train/all", escala)#Poner path a una carpeta con todas las immagenes de train
+                images_test, d_test =  cargar_datos("/Users/nicolasfranke/Downloads/chest_xray/test/all", escala)
+                np.random.seed(seed)
+                b = np.random.randn(1)
+                w = np.random.randn(images[0].shape[0])
+                print("imagenes cargadas")
+                images_balanceadas, d_balanceado = balancear_datos(images, d)
+                w_estrella, b_estrella, errores_test, errores_train = gradiente_descendente(w, b, images_balanceadas, d_balanceado, alpha,images_test, d_test)
 
-            plot_error_curve(errores_train, alpha, "train", escala, seed)
-            plot_error_curve(errores_test, alpha, "test", escala, seed)
-            matriz_confusion(images_test,d_test, b_estrella,w_estrella,escala,alpha,seed)
+                plot_error_curve(errores_train, alpha, "train", escala, seed)
+                plot_error_curve(errores_test, alpha, "test", escala, seed)
+                matriz_confusion(images_test,d_test, b_estrella,w_estrella,escala,alpha,seed)
 
-            # print(f"Valor inicial de b: {b}")
-            # print(f"Valor inicial de w: {w}")
+                print(f"Valor inicial de b: {b}")
+                print(f"Valor inicial de w: {w}")
 
 if __name__ == "__main__":
     main()
